4cf44ed9a044a9bcd9865dcb5c3c9582d8e0844f
daitokojima <daito.kojima@salto.link>
2024-06-25 09:02:45 +0900

    added links to my socials

--- components/Social.jsx
+++ components/Social.jsx
@@ -3,10 +3,10 @@ import Link from "next/link"
 import { FaGithub, FaLinkedinIn, FaYoutube, FaTwitter } from 'react-icons/fa'
 
 const socials = [
-    { icon: <FaGithub />, path: "/" },
-    { icon: <FaLinkedinIn />, path: "/" },
-    { icon: <FaYoutube />, path: "/" },
-    { icon: <FaTwitter />, path: "/" },
+    { icon: <FaGithub />, path: "https://github.com/daitok6" },
+    { icon: <FaLinkedinIn />, path: "https://www.linkedin.com/" },
+    { icon: <FaYoutube />, path: "https://www.youtube.com/" },
+    { icon: <FaTwitter />, path: "https://x.com/" },
 ]
 
 const Social = ({ containerStyles, iconStyles }) => {
